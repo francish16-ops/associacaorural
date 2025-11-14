6f00f649b6290244bf5c8b33693364fc8e25d316
Francis Henrique <francis.h.16@gmail.com>
2025-11-13 21:41:15 -0300

    correção

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,8 @@
-const CACHE_NAME = 'gestao-rural-cache-v22'; // Versão do cache incrementada
+const CACHE_NAME = 'gestao-rural-cache-v23'; // Versão do cache incrementada
 const urlsToCache = [
   '/',
   '/index.html',
   '/index.css',
-  '/manifest.json',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500&display=swap'
